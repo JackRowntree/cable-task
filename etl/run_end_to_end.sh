f9b3244fc325609b20a0d6de3cc7199d6ed51203
JackRowntree <jack.rowntree.1@gmail.com>
2023-01-22 12:36:50 +0000

    bash print statements

--- etl/run_end_to_end.sh
+++ etl/run_end_to_end.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
+printf "##########\nRunning unit tests\n##########\n"
 pytest test
+printf "##########\nRunning ETL\n##########\n"
 python run_etl.py
+printf "##########\nQuerying insights in output db\n##########\n"
 python show_results.py
